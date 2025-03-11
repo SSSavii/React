4f5b2a6de4934dda77cf351dda90e737d8785375
SSSavii <georgiii3512@mail.ru>
2025-03-11 22:57:58 +0700

    add import

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
 const Content = ({ activeLab }) => {
   if (!activeLab) {
     return (
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,23 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-
+import React from 'react';
 const Menu = ({ labs, activeLab, onLabSelect }) => {
-  return (
-    <div className="menu">
-      <h3>Лабораторные работы</h3>
-      <ul>
-        {labs.map(lab => (
-          <li 
-            key={lab.id} 
-            className={activeLab && activeLab.id === lab.id ? 'active' : ''}
-            onClick={() => onLabSelect(lab)}
-          >
-            {lab.id}: {lab.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default Menu;
+    return (
+      <div className="menu">
+        <h3>Лабораторные работы</h3>
+        <ul>
+          {labs.map(lab => (
+            <li 
+              key={lab.id} 
+              className={activeLab && activeLab.id === lab.id ? 'active' : ''}
+              onClick={() => onLabSelect(lab)}
+            >
+              {lab.id}: {lab.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Menu;
